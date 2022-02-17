2eb0dda999e9bba3d9d31c8beafb61f981442cc3
wangbingyi <1041493283@qq.com>
2022-02-17 17:48:15 +0800

    update

--- react-webpack-demo/src/flooks-03.js
+++ react-webpack-demo/src/flooks-03.js
@@ -16,7 +16,7 @@ function Counter() {
 }
 
 function Counter2() {
-  const { count, add, addAsync, x } = use$counter(); // 可以发现，多次调用这个hook，使用的是同一个状态
+  const { count, add, addAsync, x } = use$counter();
 
   return (
     <div>
